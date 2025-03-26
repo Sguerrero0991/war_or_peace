95cbee1435fed82f8af931e00d4e02723428ebbd
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-26 18:31:03 -0500

    Add #high_ranking_cards tests and working method

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,5 +7,17 @@ class Deck
 
   def rank_of_card_at(card_index) #method will take one argument that reps the index position of a card to be used 
     @cards[card_index].rank #This was confusing but I put in a pry and saw what I had access to
-  end           #I found @cards[0].rank worked but the () from thr argument were throwing me off 
-end
+  end           #I found @cards[0].rank returned what I wanted but the () from the argument thru me off 
+                #I just needed to replace the 0 with [card_index] to match the argument variable we'll be pasing through  
+  def high_ranking_cards
+    @cards.find_all do |card| #had to find the right enumerable for this. find_all returns an array of all elements
+      card.rank >= 11       #that match a condition 
+    end
+  end
+
+
+
+
+
+
+end   
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -24,7 +24,32 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
 
     expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it '#high_ranking_cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)  
+    card3 = Card.new(:heart, 'Ace', 14)  
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  xit '#percent_high_ranking' do
 
   end
+
+  xit '#remove_card' do
+
+  end
+
+  xit '#add_card' do
+
+  end
+
+
 
 end
