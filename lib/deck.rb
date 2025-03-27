6ac4926f47975fd198df34a6de131f373882e52f
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-26 19:07:48 -0500

    Add the #percent_high_ranking tests and its working method

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,19 +5,19 @@ class Deck
     @cards = cards
   end
 
-  def rank_of_card_at(card_index) #method will take one argument that reps the index position of a card to be used 
+  def rank_of_card_at(card_index) #method will take one argument that reps the index position of card being passed 
     @cards[card_index].rank #This was confusing but I put in a pry and saw what I had access to
   end           #I found @cards[0].rank returned what I wanted but the () from the argument thru me off 
-                #I just needed to replace the 0 with [card_index] to match the argument variable we'll be pasing through  
+                #I just needed to replace the 0 with [card_index] to match the argument variable that's passed thru
   def high_ranking_cards
     @cards.find_all do |card| #had to find the right enumerable for this. find_all returns an array of all elements
-      card.rank >= 11       #that match a condition 
+      card.rank >= 11       #that match the written condition 
     end
   end
 
-
-
-
-
+  def percent_high_ranking #this method will return the percentage of cards that are high ranking
+    (high_ranking_cards.count.to_f / @cards.count.to_f).round(4) * 100 #this was a doozy. Pry til you die! Also helped to waterfall through previous methods and build on them. Learned a lot in this method. 
+    #learned I could use .count on a method, and that I have to convert a number to_f and then round it to the decimal point I want. In this case (4)
+  end
 
 end   
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -38,11 +38,18 @@ RSpec.describe Deck do
     expect(deck.high_ranking_cards).to eq([card1, card3])
   end
 
-  xit '#percent_high_ranking' do
+  it '#percent_high_ranking' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)  
+    card3 = Card.new(:heart, 'Ace', 14)  
+    cards = [card1, card2, card3]
 
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
   end
 
-  xit '#remove_card' do
+  it '#remove_card' do
 
   end
 
