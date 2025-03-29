c38edbe34e38f3447314e0a6f48a9b67cdcc0251
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-28 21:55:24 -0500

    Will create test to initialize a Player

---
+++ spec/player_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require 'pry'
+
+RSpec.describe Player do 
+  it 'exists' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)    
+    card3 = Card.new(:heart, 'Ace', 14)    
+
+    deck = Deck.new([card1, card2, card3])
+
+    player = Player.new('Clarisa', deck)
+
+    expect(player).to be_a(Player)
+  end
+end
